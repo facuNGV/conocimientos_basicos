ef62e3006c8d438ec5dce86f4f32b6e3144d030f
Facuindo <facu_nicogas@outlook.com>
2023-10-29 01:16:13 -0300

    Mayor y menor de una lista numerica random utilizando listcomp

--- funciones.py
+++ funciones.py
@@ -1,7 +1,7 @@
 """
 Un par de funciones de práctica 
 """
-
+import random
 
 def porcentajem2(m2totales, m2cubiertos):
     m2descubiertos = m2totales - m2cubiertos
@@ -20,9 +20,26 @@ def descuento_condicionado(importe_venta):
     return importe_final
 
 
+def may_men():
+    numeros = [random.randint(0,100) for x in range(10)]
+    for x in numeros:
+
+        if x == numeros[1] and x > numeros[0]:
+                mayor = x
+                menor = numeros[0]
+        elif x == numeros[1] and x < numeros[0]:
+            mayor = numeros[0]
+            menor = x
+        elif x != numeros[0]:
+            if x > mayor:
+                mayor = x
+            elif x < menor:
+                menor = x
+         
 
 
 
 if __name__ == '__main__':
-    porcentajem2()
-    descuento_condicionado()
+    # porcentajem2()
+    # descuento_condicionado()
+    may_men()
